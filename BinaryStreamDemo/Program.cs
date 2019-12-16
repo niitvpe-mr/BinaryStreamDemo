91154c7624f3f4f128fdf36b7274c13f4d3e6233
niitvpe.mr <niitvpe.mr@gmail.com>
2019-12-16 11:38:22 +0530

    Binary File Demo

--- BinaryStreamDemo/Program.cs
+++ BinaryStreamDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace BinaryStreamDemo
 {
@@ -6,7 +7,45 @@ namespace BinaryStreamDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryWriter dataOut;
+            BinaryReader dataIn;
+            int i = 10;
+            double d = 56.23;
+            bool b = true;
+
+            dataOut = new BinaryWriter(new FileStream("testData",FileMode.Create));
+            Console.WriteLine("Writing "+i);
+            dataOut.Write(i);
+
+            Console.WriteLine("Writing "+d);
+            dataOut.Write(d);
+
+            Console.WriteLine("Writing " + b);
+            dataOut.Write(b);
+
+            Console.WriteLine("Writing "+12.2*8.3);
+            dataOut.Write(12.2 * 8.3);
+
+            dataOut.Close();
+            Console.WriteLine();
+
+            dataIn = new BinaryReader(new FileStream("testData",FileMode.Open));
+
+            i = dataIn.ReadInt32();
+            Console.WriteLine("Reading "+i);
+
+            d = dataIn.ReadDouble();
+            Console.WriteLine("Reading "+d);
+
+            b = dataIn.ReadBoolean();
+            Console.WriteLine("Reading "+b);
+
+            d = dataIn.ReadDouble();
+            Console.WriteLine("Reading " + d);
+
+            Console.ReadLine();
+            dataIn.Close();
+
         }
     }
 }
